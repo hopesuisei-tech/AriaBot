b9999c923a276cb50bb8b8197002bb78a9877f35
hopesuisei-tech <hope.suisei@gmail.com>
2026-07-29 18:33:38 +0200

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,8 +24,8 @@ export const botConfig = {
     activities: [
       {
         name: "Custom Status", // required by Discord API, not shown in the client
-        state: "stalking",     // this is what people actually see
-        type: 4,               // Custom
+        state: "La Morte Di Rice",     // this is what people actually see
+        type: 3,               // Custom
       },
     ],
   },
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#336699",
-      secondary: "#2F3136",
+      primary: "#FFDC73",
+      secondary: "#FFFFFF",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
